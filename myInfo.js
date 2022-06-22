f94df62d73c855cdc0af0bd4981b9048ff56d6a3
Olayinka JOHN <bisoye.jy@gmail.com>
2022-06-22 20:06:03 +0100

    A JavaScript code that prints out my name, height, and country on the screen

---
+++ myInfo.js
@@ -0,0 +1,14 @@
+alert("My name is IDOWU OLAYINKA. My height is 156cm and the name of my country is NIGERIA.");
+
+// alert('My name is IDOWU OLAYINKA, My height is 156cm,
+//                The name of my country is NIGERIA.
+//           ')
+
+let myName = 'Name: IDOWU OLAYINKA'
+let height = 'Height: 156cm'
+let country  = 'Country: NIGERIA'
+
+console.log(myName);
+console.log(height);
+console.log(country);
+
